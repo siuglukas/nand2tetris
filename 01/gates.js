9846ab5d47f51332a503b5413d1e5ef25a78c885
Lukas <siug.lukas@gmail.com>
2021-07-08 21:32:11 +0300

    16bit variants

---
+++ 01/gates.js
@@ -0,0 +1,6 @@
+(function generateGates(name, size) {
+  for (i = 0; i !== size; i++) {
+    // console.log(`${name}(a=a[${i}], b=b[${i}], sel=sel, out=out[${i}]);`);
+    // console.log(`${name}(a=a[${i}], b=b[${i}], out=out[${i}]);`);
+  }
+})("Mux", 16);
