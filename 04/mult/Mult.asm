76473eca0a41bae15a4714e4cc304efda166bf28
Lukas <siug.lukas@gmail.com>
2021-07-17 21:49:56 +0300

    assembly multiplication

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,88 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// @sum 
+// M=0
+// @R0 
+// D=M
+// @REG0 
+// M=D
+
+// (LOOP)
+// @R1    
+// D=M  
+// @sum
+// M=M+D 
+// @REG0  
+// M=M-1
+// D=M
+
+// @R2
+// M=0
+
+// @LOOP
+// D;JGT
+
+// @sum
+// D=M
+// @R2
+// M=D
+
+// (END)
+// @END
+
+// 0;JMP
+
+
+// 5*3 in pseudocode:
+
+// sum = 0
+// register0 = 5 
+// register1 = 3
+
+// for(i=5; i != 0; i--) {   
+//    sum += 
+// }
+
+
+
+@sum 
+M=0 // sum = 0
+@R0 
+D=M
+@REG0 
+M=D // REG0 = RAM[0]
+
+(LOOP)
+
+
+// If REG0 == 0, exit the loop.
+@REG0
+@STOP
+M;JEQ
+
+// else
+@R1    
+D=M  
+@sum
+M=M+D // sum += register1
+@REG0  
+M=M-1 // REG0--
+
+
+@LOOP
+0;JMP
+
+
+// Store the result in register2 and exit the program. 
+(STOP)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
+
